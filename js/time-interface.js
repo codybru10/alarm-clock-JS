1cd36764dbe7091a2a1e35729fc6bc28ad434cf6
Cody Brubaker and Joshua Rinard <cody@>
2017-01-03 13:06:14 -0800

    interval time

--- js/time-interface.js
+++ js/time-interface.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
   setInterval(function(){
-    document.getElementById("time").innerHTML = moment().format('dddd YYYY, LTS');
+    $("#time").text(moment().format('dddd YYYY, LTS'));
   }, 1000);
 
 });
